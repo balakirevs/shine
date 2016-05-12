6d0c40bfeab5d88746f6a5cae0e03ca60b425d16
Aleksandr Balakiriev <balakirevs@i.ua>
2016-05-12 22:08:36 +0200

    Clean search results, pagination with bootstrap components

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,12 @@
 class CustomersController < ApplicationController 
-	def index
+	PAGE_SIZE = 10
+
+  def index
+    @page = (params[:page] || 0).to_i
     if params[:keywords].present?
 			@keywords = params[:keywords]
 			customer_search_term = CustomerSearchTerm.new(@keywords) 
-			@customers = Customer.where(customer_search_term.where_clause, customer_search_term.where_args).order(customer_search_term.order)
+			@customers = Customer.where(customer_search_term.where_clause, customer_search_term.where_args).order(customer_search_term.order).offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
 		end
