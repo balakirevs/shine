ca6842498b8f6347c86f135595aba2c82538626b
Aleksandr Balakiriev <balakirevs@i.ua>
2017-08-06 23:49:45 +0200

    Karma jasmine testdouble setup & config

---
+++ spec/javascript/karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function(config) {
+ config.set({
+   frameworks: ['jasmine'],
+   files: [
+     '**/*.spec.js'
+   ],
+   preprocessors: {
+     '**/*.spec.js': [ 'webpack' ]
+   },
+   webpack: require('../../config/webpack/test.js'),
+   browsers: ['PhantomJS'] })
+};
