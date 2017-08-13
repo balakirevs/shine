c044fbe2d0e061d275007fa3e376c9ccec7b9092
Aleksandr Balakiriev <balakirevs@i.ua>
2017-08-13 11:12:29 +0200

    Implements search by username & fullname

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -1,5 +1,5 @@
 class CustomerSearchTerm
-  attr_reader :where_clause, :where_args, :order
+  attr_reader :where_clause, :where_args, :order, :first_term, :second_term
 
   def initialize(search_term)
     search_term = search_term.downcase
@@ -7,8 +7,10 @@ class CustomerSearchTerm
     @where_args = {}
     if search_term =~ /@/
       build_for_email_search(search_term)
-    else
+    elsif single_search_term?(search_term)
       build_for_name_search(search_term)
+    else
+      build_for_full_name_search(search_term)
     end
   end
 
@@ -19,7 +21,30 @@ class CustomerSearchTerm
     @where_args[:first_name] = starts_with(search_term)
     @where_clause << " OR #{case_insensitive_search(:last_name)}"
     @where_args[:last_name] = starts_with(search_term)
+    @where_clause << " OR #{case_insensitive_search(:username)}"
+    @where_args[:username] = starts_with(search_term)
     @order = 'last_name asc'
+  end
+
+  def build_for_full_name_search(search_term)
+    @first_term = truncate_first_term(search_term)
+    @second_term = truncate_second_term(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(@first_term)
+    @where_clause << " AND #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(@second_term)
+  end
+
+  def single_search_term?(search_term)
+    !search_term.strip.include? ' '
+  end
+
+  def truncate_first_term(search_term)
+    search_term.split(' ')[0..0].join(' ')
+  end
+
+  def truncate_second_term(search_term)
+    search_term.split(' ')[1..1].join(' ')
   end
 
   def starts_with(search_term)
--- spec/features/customer_search_spec.rb
+++ spec/features/customer_search_spec.rb
@@ -8,24 +8,19 @@ feature 'Customer Search' do
   before do
     create_test_user(email: email, password: password)
 
-    create_customer first_name: 'Chris', last_name: 'Aaron'
-    create_customer first_name: 'Pat', last_name: 'Johnson'
-    create_customer first_name: 'I.T.', last_name: 'Pat'
-    create_customer first_name: 'Patricia', last_name: 'Dobbs'
+    create_customer first_name: 'Chris', last_name: 'Aaron', username: 'chris'
+    create_customer first_name: 'Pat', last_name: 'Johnson', username: 'patjohn'
+    create_customer first_name: 'I.T.', last_name: 'Pat', username: 'patit'
+    create_customer first_name: 'Patricia', last_name: 'Dobbs', username: 'patricia'
 
     # This user is the one we'll expect to be listed first
-    create_customer first_name: 'Pat',
-                    last_name: 'Jones',
-                    email: 'pat123@somewhere.net'
+    create_customer first_name: 'Pat', last_name: 'Jones', username: 'pat123', email: 'pat123@somewhere.net'
   end
 
   scenario 'Search by Name' do
     visit '/customers'
 
-    # Login to get access to /customers
-    fill_in 'Email',    with: email
-    fill_in 'Password', with: password
-    click_button 'Log in'
+    log_in(email, password)
 
     within 'section.search-form' do
       fill_in 'keywords', with: 'pat'
@@ -44,13 +39,54 @@ feature 'Customer Search' do
     end
   end
 
+  scenario 'Search by User Name' do
+    visit '/customers'
+
+    log_in(email, password)
+
+    within 'section.search-form' do
+      fill_in 'keywords', with: 'pat'
+    end
+
+    within 'section.search-results' do
+      expect(page).to have_content('Results')
+      expect(page.all('ol li.list-group-item').count).to eq(4)
+
+      list_group_items = page.all('ol li.list-group-item')
+
+      expect(list_group_items[0]).to have_content('patricia')
+      expect(list_group_items[0]).to have_content('Dobbs')
+      expect(list_group_items[3]).to have_content('patit')
+      expect(list_group_items[3]).to have_content('Pat')
+    end
+  end
+
+  scenario 'Search by Full Name' do
+    visit '/customers'
+
+    log_in(email, password)
+
+    within 'section.search-form' do
+      fill_in 'keywords', with: 'Patricia Dobbs'
+    end
+
+    within 'section.search-results' do
+      expect(page).to have_content('Results')
+      expect(page.all('ol li.list-group-item').count).to eq(1)
+
+      list_group_items = page.all('ol li.list-group-item')
+
+      expect(list_group_items[0]).to have_content('Patricia')
+      expect(list_group_items[0]).to have_content('Dobbs')
+      expect(list_group_items[3]).not_to have_content('I.T.')
+      expect(list_group_items[3]).not_to have_content('Pat')
+    end
+  end
+
   scenario 'Search by Email' do
     visit '/customers'
 
-    # Login to get access to /customers
-    fill_in 'Email',    with: email
-    fill_in 'Password', with: password
-    click_button 'Log in'
+    log_in(email, password)
 
     within 'section.search-form' do
       fill_in 'keywords', with: 'pat123@somewhere.net'
